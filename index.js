b9068cf4622057821f9f04eefc0d200cd2a4b787
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-26 11:03:02 +0700

    data

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Server started'));
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => {
+    //truy van database lay ra tat ca cac san pham
+    
+});
