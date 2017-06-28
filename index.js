209f3bc0d73fe6bba09e4afdcca7cabb88625b4b
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-28 09:24:46 +0700

    delete a row

--- index.js
+++ index.js
@@ -42,3 +42,12 @@ app.get('/admin', (req, res) => {
         res.render('admin', { arrProducts: result.rows });
     });
 });
+
+app.get('/xoa/:id', (req, res) => {
+    const { id } = req.params;
+    const deleteSql = 'DELETE FROM "Product" WHERE id = ' + id;
+    queryDB(deleteSql, (err, result) => {
+        if (err) return res.send('Xoa khong thanh cong');
+        res.redirect('/admin');
+    });
+});
