5f2ec37bc1adfafb891d5513f1096890c7ae2ef2
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-28 10:21:45 +0700

    tach file

--- index.js
+++ index.js
@@ -10,16 +10,9 @@ app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Server started'));
 
-app.get('/', (req, res) => {
-    queryDB('SELECT * FROM "Product"', (err, result) => {
-        if (err) return res.send('LOI');
-        res.render('home', { arrProducts: result.rows });
-    });
-});
+app.get('/', require('./controllers/homeController'));
 
-app.get('/addProduct', (req, res) => {
-    res.render('add');
-});
+app.get('/addProduct', (req, res) => res.render('add'));
 
 app.post('/addProduct', (req, res) => {
     upload(req, res, err => {
