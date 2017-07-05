9acc15ed3a373f16d6eded22523f20645a6db363
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-05 11:30:07 +0700

    update sth

--- db.js
+++ db.js
@@ -1,19 +1,23 @@
 const pg = require('pg');
 
 const pool = new pg.Pool({
-    host: 'localhost',
+    host: 'ec2-107-22-162-158.compute-1.amazonaws.com',
     port: 5432,
-    database: 'NODE0806',
-    password: 'khoapham',
-    user: 'postgres',
-    max: 10,
-    idleTimeoutMillis: 1000
+    database: 'db69lmp0ur7kd6',
+    password: 'bd1ad97333f8c5a2b20222c30bde770399365787b466d23fb8c49cd6e9255200',
+    user: 'jvqaetvfkrblop',
+    max: 20,
+    idleTimeoutMillis: 1000,
+    ssl: true
 });
 
 function queryDB(sql, cb) {
-    pool.connect((err, client) => {
+    pool.connect((err, client, done) => {
         if (err) return cb(err);
-        client.query(sql, cb);
+        client.query(sql, (errQuery, result) => {
+            done(errQuery);
+            cb(errQuery, result);
+        });
     });
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-// const queryDB = require('./db');
-// const upload = require('./uploadConfig').single('image');
+const queryDB = require('./db');
+const upload = require('./uploadConfig').single('image');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -8,11 +8,11 @@ app.set('views', './views');
 app.use(express.static('public'));
 app.listen(process.env.PORT || 3000, () => console.log('Server started'));
 
-// app.get('/', require('./controllers/homeController'));
-// app.get('/addProduct', (req, res) => res.render('add'));
-// app.post('/addProduct', require('./controllers/addProductController'));
-// app.get('/admin', require('./controllers/adminController'));
-// app.get('/xoa/:id', require('./controllers/removeController'));
-// app.get('/sua/:id', require('./controllers/updateViewController'));
-// app.post('/update', require('./controllers/updateController'));
-app.get('/', (req, res) => res.send('server is running'));
+app.get('/', require('./controllers/homeController'));
+app.get('/addProduct', (req, res) => res.render('add'));
+app.post('/addProduct', require('./controllers/addProductController'));
+app.get('/admin', require('./controllers/adminController'));
+app.get('/xoa/:id', require('./controllers/removeController'));
+app.get('/sua/:id', require('./controllers/updateViewController'));
+app.post('/update', require('./controllers/updateController'));
+// app.get('/', (req, res) => res.send('server is running'));
