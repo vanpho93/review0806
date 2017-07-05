407530769af343a079e89fdd68f35d9413026ea0
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-05 10:51:07 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const queryDB = require('./db');
-const upload = require('./uploadConfig').single('image');
+// const queryDB = require('./db');
+// const upload = require('./uploadConfig').single('image');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
-app.listen(3000, () => console.log('Server started'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
 
-app.get('/', require('./controllers/homeController'));
-app.get('/addProduct', (req, res) => res.render('add'));
-app.post('/addProduct', require('./controllers/addProductController'));
-app.get('/admin', require('./controllers/adminController'));
-app.get('/xoa/:id', require('./controllers/removeController'));
-app.get('/sua/:id', require('./controllers/updateViewController'));
-app.post('/update', require('./controllers/updateController'));
+// app.get('/', require('./controllers/homeController'));
+// app.get('/addProduct', (req, res) => res.render('add'));
+// app.post('/addProduct', require('./controllers/addProductController'));
+// app.get('/admin', require('./controllers/adminController'));
+// app.get('/xoa/:id', require('./controllers/removeController'));
+// app.get('/sua/:id', require('./controllers/updateViewController'));
+// app.post('/update', require('./controllers/updateController'));
+app.get('/', (req, res) => res.send('server is running'));
